a675c181a255ff5c8ac71804c8cc9cb23e29b5ce
Toshiya Takahashi <toshiya2113@gmail.com>
2023-09-02 22:05:20 +0900

    chapter_1_end

---
+++ laravel-study/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function create(){
+        return view('regist.register');
+    }
+
+    public function store(Request $request){
+
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        return view('regist.complete',compact('user'));
+    }
+}
